527ebe7d443cc0dfded646d871f3f2138a40e1bd
Ali Shkeir <alishkeir_@hotmail.com>
2021-04-26 03:41:23 +0300

    created meteor app with svelte

---
+++ client/main.jsx
@@ -0,0 +1,10 @@
+import { Meteor } from "meteor/meteor"; 
+import App from "../imports/App.svelte";
+
+Meteor.startup(() => {
+  new App({
+    target: document.getElementById("app"),
+  });
+
+  // render(<App />, document.getElementById("app"));
+});
---
+++ imports/App.svelte
@@ -0,0 +1,3 @@
+<header>
+  <h1>Yooo!!</h1>
+</header>
