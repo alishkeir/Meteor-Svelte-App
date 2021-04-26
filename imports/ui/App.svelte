59134532b08dc165d00cc4387f3f274c58b4c470
Ali Shkeir <alishkeir_@hotmail.com>
2021-04-26 04:10:12 +0300

    Insert Data to MongoDB Via Form in Svelte

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -2,13 +2,29 @@
   import { useTracker } from "meteor/rdb:svelte-meteor-data";
   import { Resolutions } from "../api/resolutions";
 
+  let newRes = "";
+
   //^ Reactive Variable
 
   $: resolutions = useTracker(() => Resolutions.find({}).fetch());
+
+  function handleSubmit(e) {
+    console.log(newRes);
+    Resolutions.insert({
+      title: newRes,
+    });
+
+    newRes = "";
+  }
 </script>
 
 <header>
   <h1>Yooo!!</h1>
+
+  <form action="" on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="Add Your Resolution" bind:value={newRes} />
+    <button type="submit">Submit</button>
+  </form>
 
   {#each $resolutions as resolution}
     <h2>{resolution.title}</h2>
