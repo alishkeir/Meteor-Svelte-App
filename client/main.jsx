1841aa48834269cca8c2817c54ef854323a10161
Ali Shkeir <alishkeir_@hotmail.com>
2021-04-26 03:59:29 +0300

    Created mongo collection and displayed on svelte file

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,8 @@
-import { Meteor } from "meteor/meteor"; 
-import App from "../imports/App.svelte";
+import { Meteor } from "meteor/meteor";
+import App from "../imports/ui/App.svelte";
 
 Meteor.startup(() => {
   new App({
     target: document.getElementById("app"),
   });
-
-  // render(<App />, document.getElementById("app"));
 });
--- imports/App.svelte
+++
@@ -1,3 +0,0 @@
-<header>
-  <h1>Yooo!!</h1>
-</header>
